1ebed0f0b4f6235281d31246f0ab04b3af7c3f47
gdantimi <gianluigi.dantimi@dxc.com>
2022-08-31 16:22:44 +0200

    creata class RicettaBean

--- src/main/java/jsonParsing/json/Main.java
+++ src/main/java/jsonParsing/json/Main.java
@@ -5,6 +5,8 @@ import org.json.JSONArray;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -12,31 +14,57 @@ public class Main {
 
         System.out.println("DXC-PIP progetto per creazione e lettura di un JSON JSON JSON");
 
+        /*
+            JSONObject elencoRicette = new JSONObject();
+
+            JSONArray ricetta = new JSONArray();
+
+
+            JSONObject ricetta1 = new JSONObject();
+            JSONObject ricetta2 = new JSONObject();
+
+            ricetta1.put("categoria", "primi");
+            ricetta1.put("nome", "pasta");
+            ricetta1.put("ingredienti", "pasta, pomodoro");
+            ricetta1.put("colore", "yellow");
+
+            ricetta2.put("categoria", "secondi");
+            ricetta2.put("nome", "carne");
+            ricetta2.put("ingredienti", "carne, sale, olio");
+            ricetta2.put("colore", "orange");
+
+
+            ricetta.put(ricetta1);
+            ricetta.put(ricetta2);
+
+            elencoRicette.put("elenco ricette", ricetta);
+
+            System.out.println(elencoRicette.toString(2));
+        */
+
+//metodo 1
+        RicettaBean ricettaBean1 = new RicettaBean(1, "primi", "pasta", "pasta, pomodoro", "yellow");
+
+        //metodo 2 usando getter+setter nel caso in cui l'array non sia completo di tutti gli elementi
+
+        RicettaBean ricettaBean2 = new RicettaBean();
+        ricettaBean2.setId(2);
+        ricettaBean2.setCategoria("secondi");
+        ricettaBean2.setNome("orata");
+        ricettaBean2.setIngredienti("orata, patate");
+        ricettaBean2.setColore("orange");
+
+        List<RicettaBean> listaRicette = new ArrayList<>();
+        listaRicette.add(ricettaBean1);
+        listaRicette.add(ricettaBean2);
+
+        JSONArray ricette = new JSONArray(listaRicette);
         JSONObject elencoRicette = new JSONObject();
 
-        JSONArray ricetta = new JSONArray();
+        elencoRicette.put("elenco ricette", ricette);
 
 
-        JSONObject ricetta1 = new JSONObject();
-        JSONObject ricetta2 = new JSONObject();
-
-        ricetta1.put("categoria", "primi");
-        ricetta1.put("nome", "pasta");
-        ricetta1.put("ingredienti", "pasta, pomodoro");
-        ricetta1.put("colore", "yellow");
-
-        ricetta2.put("categoria", "secondi");
-        ricetta2.put("nome", "carne");
-        ricetta2.put("ingredienti", "carne, sale, olio");
-        ricetta2.put("colore", "orange");
-
-
-        ricetta.put(ricetta1);
-        ricetta.put(ricetta2);
-
-        elencoRicette.put("elenco ricette", ricetta);
-
-        System.out.println(elencoRicette.toString(2));
+        System.out.println(ricette.toString(2));
 
         try (FileWriter file = new FileWriter("elencoRicette.json")) {
             elencoRicette.write(file, 4, 0);
---
+++ src/main/java/jsonParsing/json/RicettaBean.java
@@ -0,0 +1,63 @@
+package jsonParsing.json;
+
+public class RicettaBean {
+
+    private int id;
+    private String categoria;
+    private String nome;
+    private String ingredienti;
+    private String colore;
+
+    public RicettaBean() {
+    }
+
+    public RicettaBean(int id, String categoria, String nome, String ingredienti, String colore) {
+        this.id = id;
+        this.categoria = categoria;
+        this.nome = nome;
+        this.ingredienti = ingredienti;
+        this.colore = colore;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIngredienti() {
+        return ingredienti;
+    }
+
+    public void setIngredienti(String ingredienti) {
+        this.ingredienti = ingredienti;
+    }
+
+    public String getColore() {
+        return colore;
+    }
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
+
+}
