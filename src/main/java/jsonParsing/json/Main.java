669ced0283faf3a75f65f4ce55973d344c54e55b
gdantimi <gianluigi.dantimi@dxc.com>
2022-08-31 18:01:01 +0200

    versione 3 - creazione json da csv

--- src/main/java/jsonParsing/json/Main.java
+++ src/main/java/jsonParsing/json/Main.java
@@ -8,17 +8,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.CDL;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("DXC-PIP progetto per creazione e lettura di un JSON JSON JSON");
+        System.out.println();
+        System.out.println("DXC-PIP progetto per creazione e lettura di un file JSON");
 
+
+        //versione 1 (creare json direttamente nel main)
         /*
+            System.out.println("creazione json direttamente nel main");
+            System.out.println();
+
             JSONObject elencoRicette = new JSONObject();
 
             JSONArray ricetta = new JSONArray();
-
 
             JSONObject ricetta1 = new JSONObject();
             JSONObject ricetta2 = new JSONObject();
@@ -33,7 +43,6 @@ public class Main {
             ricetta2.put("ingredienti", "carne, sale, olio");
             ricetta2.put("colore", "orange");
 
-
             ricetta.put(ricetta1);
             ricetta.put(ricetta2);
 
@@ -42,30 +51,58 @@ public class Main {
             System.out.println(elencoRicette.toString(2));
         */
 
-//metodo 1
-        RicettaBean ricettaBean1 = new RicettaBean(1, "primi", "pasta", "pasta, pomodoro", "yellow");
 
-        //metodo 2 usando getter+setter nel caso in cui l'array non sia completo di tutti gli elementi
+        //versione 2 (creare json utilizzando la classe Bean)
+        /*
+            System.out.println("creazione json utilizzando classe Bean");
+            System.out.println();
 
-        RicettaBean ricettaBean2 = new RicettaBean();
-        ricettaBean2.setId(2);
-        ricettaBean2.setCategoria("secondi");
-        ricettaBean2.setNome("orata");
-        ricettaBean2.setIngredienti("orata, patate");
-        ricettaBean2.setColore("orange");
+            //metodo 1
+            RicettaBean ricettaBean1 = new RicettaBean(1, "primi", "pasta", "pasta, pomodoro", "yellow");
 
-        List<RicettaBean> listaRicette = new ArrayList<>();
-        listaRicette.add(ricettaBean1);
-        listaRicette.add(ricettaBean2);
+            //metodo 2 usando getter+setter nel caso in cui l'array non sia completo di tutti gli elementi
 
-        JSONArray ricette = new JSONArray(listaRicette);
-        JSONObject elencoRicette = new JSONObject();
+            RicettaBean ricettaBean2 = new RicettaBean();
+            ricettaBean2.setId(2);
+            ricettaBean2.setCategoria("secondi");
+            ricettaBean2.setNome("orata");
+            ricettaBean2.setIngredienti("orata, patate");
+            ricettaBean2.setColore("orange");
 
-        elencoRicette.put("elenco ricette", ricette);
+            List<RicettaBean> listaRicette = new ArrayList<>();
+            listaRicette.add(ricettaBean1);
+            listaRicette.add(ricettaBean2);
+
+            JSONObject elencoRicette = new JSONObject();
+            JSONArray ricette = new JSONArray(listaRicette);
+
+            elencoRicette.put("elenco ricette", ricette);
+
+            System.out.println(ricette.toString(2));
+*/
 
 
-        System.out.println(ricette.toString(2));
+        //versione 3 (creare json da un file .csv)
+        System.out.println("creazione json da un file .csv");
+        System.out.println();
 
+        JSONArray headerRicette = new JSONArray();
+
+        headerRicette.put("id");
+        headerRicette.put("categoria");
+        headerRicette.put("nome");
+        headerRicette.put("ingredienti");
+        headerRicette.put("colore");
+
+        String ricetteCSV = new String(Files.readAllBytes(Paths.get("ricetteCSV.csv")));
+        System.out.println(ricetteCSV);
+        System.out.println();
+
+        JSONArray elencoRicette = CDL.toJSONArray(headerRicette, ricetteCSV);
+
+        System.out.println(elencoRicette.toString(2));
+
+        //ok per tutte le versioni
         try (FileWriter file = new FileWriter("elencoRicette.json")) {
             elencoRicette.write(file, 4, 0);
         } catch (IOException e) {
