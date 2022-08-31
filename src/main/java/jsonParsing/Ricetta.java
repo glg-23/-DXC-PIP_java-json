9e51b4f8dd16c9be2f78df11bb4b15a1b3d4cb3e
gdantimi <gianluigi.dantimi@dxc.com>
2022-08-31 15:05:24 +0200

    sostituito JSONAware con JSONStreamAware

--- src/main/java/jsonParsing/Ricetta.java
+++ src/main/java/jsonParsing/Ricetta.java
@@ -1,8 +1,15 @@
 package jsonParsing;
 
-import org.json.simple.JSONAware;
+import org.json.simple.JSONStreamAware;
+import org.json.simple.JSONValue;
 
-public class Ricetta implements JSONAware {
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.Writer;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Ricetta implements JSONStreamAware {
 
     private int numElemento;
     private String categoria;
@@ -18,14 +25,28 @@ public class Ricetta implements JSONAware {
         this.colore = colore;
     }
 
-    @Override
-    public String toJSONString() {
-        return String.format("{\"numElemento\":%d," +
-                        "\"categoria\":\"%s\"," +
-                        "\"nome\":\"%s\"," +
-                        "\"ingredienti\":\"%s\"," +
-                        "\"colore\":\"%s\"}",
-                numElemento, categoria, nome, ingredienti, colore);
-    }
+    /*
+        @Override
+        public String toJSONString() {
+            return String.format("{\"numElemento\":%d," +
+                            "\"categoria\":\"%s\"," +
+                            "\"nome\":\"%s\"," +
+                            "\"ingredienti\":\"%s\"," +
+                            "\"colore\":\"%s\"}",
+                    numElemento, categoria, nome, ingredienti, colore);
+        }
+    */
 
+    @Override
+    public void writeJSONString(Writer writer) throws IOException{
+        Map<String, Serializable> map = new LinkedHashMap<String, Serializable>();
+
+        map.put("numElemento",numElemento);
+        map.put("categoria",categoria);
+        map.put("nome",nome);
+        map.put("ingredienti",ingredienti);
+        map.put("colore",colore);
+
+        JSONValue.writeJSONString(map, writer);
+    }
 }
